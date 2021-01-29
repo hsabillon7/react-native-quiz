e9685c7f98b199b6eecbfadc92acaa6e62c82977
Héctor Sabillón <h.hsabillon@gmail.com>
2021-01-28 19:55:49 -0600

    Implementación de los hooks de estado y de efectos secundarios

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import fetchQuizQuestion from "./src/api";
 
 export default function App() {
+  const [questions, setQuestions] = useState([]);
+
+  // componentDidMount
+  useEffect(() => {
+    const getQuestions = async () => {
+      const questions = await fetchQuizQuestion(10, "easy");
+
+      return questions;
+    };
+
+    const newQuestions = getQuestions();
+    setQuestions(newQuestions);
+  }, []);
+
+  if (questions.length) {
+    return (
+      <View style={styles.container}>
+        <Text>React Native Quiz App</Text>
+        <Text>{questions[0].question}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>React Native Quiz App</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
