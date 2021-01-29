01786d1fd0a014ea94450d090d9072423c227a53
Héctor Sabillón <h.hsabillon@gmail.com>
2021-01-28 19:55:25 -0600

    Configuración de la API hacia opentbd.com. El valor de retorno agrega un arreglo que contiene las respuestas correctas y la respuesta incorrecta

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+const fetchQuizQuestion = async (amount, difficulty, category = "") => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&category=${category}&type=multiple`;
+
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Map
+  // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Operadores/Spread_operator
+  return data.results.map((question) => ({
+    ...question,
+    answers: [...question.incorrect_answers, question.correct_answer],
+  }));
+};
+
+export default fetchQuizQuestion;
