7f66e9db19b9fac5ac4059e68e05e1c4e3bce33e
Héctor Sabillón <h.hsabillon@gmail.com>
2021-02-01 19:53:57 -0600

    Implementación del componente QuestionCard

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import fetchQuizQuestion from "./src/api";
+import QuestionCard from "./src/components/QuestionCard";
 
 export default function App() {
   const [questions, setQuestions] = useState([]);
@@ -9,20 +10,25 @@ export default function App() {
   // componentDidMount
   useEffect(() => {
     const getQuestions = async () => {
-      const questions = await fetchQuizQuestion(10, "easy");
+      const newQuestions = await fetchQuizQuestion(10, "easy");
 
-      return questions;
+      setQuestions(newQuestions);
     };
 
-    const newQuestions = getQuestions();
-    setQuestions(newQuestions);
+    getQuestions();
   }, []);
 
   if (questions.length) {
     return (
       <View style={styles.container}>
         <Text>React Native Quiz App</Text>
-        <Text>{questions[0].question}</Text>
+        <QuestionCard
+          question={questions[0].question}
+          answers={questions[0].answers}
+          questionNumber={1}
+          totalQuestions={10}
+          correctAnswer={questions[0].correct_answer}
+        />
         <StatusBar style="auto" />
       </View>
     );
@@ -39,7 +45,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#252c4a",
     alignItems: "center",
     justifyContent: "center",
   },
