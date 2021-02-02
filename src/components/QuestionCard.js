4baf4f1485d4f8214684dc69fb430500a12b49cc
Héctor Sabillón <h.hsabillon@gmail.com>
2021-02-01 19:53:32 -0600

    Creación del componente que se encarga de mostrar la cuenta de las preguntas, la pregunta y las distintas respuestas disponibles

---
+++ src/components/QuestionCard.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+
+const QuestionCard = ({
+  question,
+  answers,
+  userAnswer,
+  questionNumber,
+  totalQuestions,
+  correctAnswer,
+  callback,
+}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.counter}>
+        Question: {questionNumber} / {totalQuestions}
+      </Text>
+      <Text style={styles.question}>{question}</Text>
+      <View>
+        {answers.map((answer) => (
+          <TouchableOpacity key={answer} style={styles.answerButton}>
+            <Text style={styles.answerButtonText}>{answer}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 15,
+    justifyContent: "center",
+  },
+  counter: {
+    fontSize: 26,
+    color: "#8a93bb",
+    marginBottom: 50,
+    paddingBottom: 10,
+    borderStyle: "dotted",
+    borderBottomWidth: 2,
+    borderColor: "#454d6e",
+  },
+  question: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 10,
+  },
+  answerButton: {
+    textAlign: "center",
+    padding: 15,
+    borderWidth: 3,
+    borderColor: "#21486a",
+    borderRadius: 20,
+    marginBottom: 5,
+  },
+  answerButtonText: {
+    fontSize: 18,
+    color: "#ffffff",
+  },
+});
+
+export default QuestionCard;
