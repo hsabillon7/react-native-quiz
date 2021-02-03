d4bd16f0c0f1db6c15eeb6dfc35498c84f588920
Héctor Sabillón <h.hsabillon@gmail.com>
2021-02-02 19:56:02 -0600

    Implementación de puntaje, game Over y navegación entre las preguntas disponibles

--- App.js
+++ App.js
@@ -3,9 +3,14 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import fetchQuizQuestion from "./src/api";
 import QuestionCard from "./src/components/QuestionCard";
+import Button from "./src/components/Button";
 
 export default function App() {
   const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+  const [score, setScore] = useState(0);
+  const [userAnswer, setUserAnswer] = useState("");
 
   // componentDidMount
   useEffect(() => {
@@ -18,26 +23,49 @@ export default function App() {
     getQuestions();
   }, []);
 
-  if (questions.length) {
-    return (
-      <View style={styles.container}>
-        <Text>React Native Quiz App</Text>
-        <QuestionCard
-          question={questions[0].question}
-          answers={questions[0].answers}
-          questionNumber={1}
-          totalQuestions={10}
-          correctAnswer={questions[0].correct_answer}
-        />
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
+  // Maneja el incremento de las preguntas disponibles
+  const handlerNextQuestion = () => {
+    const nextNumber = number + 1;
+
+    if (nextNumber === 10) setGameOver(true);
+
+    setNumber(nextNumber);
+  };
+
+  // Verificar si la respuesta seleccionada es la correcta
+  const handlerCheckAnswer = (answer) => {
+    const correct = questions[number].correct_answer === answer;
+
+    console.log(correct);
+
+    if (correct) setScore(score + 1);
+
+    setUserAnswer(answer);
+  };
 
   return (
     <View style={styles.container}>
-      <Text>React Native Quiz App</Text>
-      <StatusBar style="auto" />
+      {!gameOver && questions.length ? (
+        <View>
+          <Text>React Native Quiz App</Text>
+          <QuestionCard
+            question={questions[number].question}
+            answers={questions[number].answers}
+            questionNumber={number + 1}
+            totalQuestions={10}
+            correctAnswer={questions[number].correct_answer}
+            callback={handlerCheckAnswer}
+          />
+          <Button title="Next" callback={handlerNextQuestion} />
+          <StatusBar style="auto" />
+        </View>
+      ) : null}
+      {gameOver ? (
+        <View>
+          <Text>Correct answers: {score}</Text>
+          <Button title="Restart" />
+        </View>
+      ) : null}
     </View>
   );
 }
