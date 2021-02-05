cdab828f40c13542189383fc443d4aad2c8db274
Héctor Sabillón <h.hsabillon@gmail.com>
2021-02-04 19:58:14 -0600

    Se agregó el título de la aplicación

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import Button from "../components/Button";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Button title="Start" />
+      <Text style={styles.title}>React Native Quiz App</Text>
+      <Button
+        title="Start"
+        callback={() => {
+          navigation.navigate("Game", { start: true });
+        }}
+      />
       <Button title="Options" />
     </View>
   );
@@ -17,6 +23,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#252c4a",
     alignItems: "center",
     justifyContent: "center",
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 20,
   },
 });
 
